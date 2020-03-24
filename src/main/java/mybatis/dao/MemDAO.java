6471a67e6e7beddcd4854e0a5279eefac1de7c14
koitt04a <koitt04a@koitt04a-PC>
2020-03-24 17:12:07 +0900

    mapper, dao 작성

--- src/main/java/mybatis/dao/MemDAO.java
+++ src/main/java/mybatis/dao/MemDAO.java
@@ -1,11 +1,70 @@
 package mybatis.dao;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.data.vo.MemberVO;
+
 public class MemDAO {
+	
 	@Autowired
 	private SqlSessionTemplate ss;
+	
+	 
+	//회원 가입 가능
+	public boolean join(MemberVO vo) {
+		boolean chk = false;
+		int cnt =  ss.insert("mem.joinMem", vo);
+	
+		if(cnt > 0) {
+			chk = true;
+		}
+		return chk;
+	}
+	
+	
+	//회원 탈퇴
+	public boolean delMem(MemberVO vo) {
+		boolean chk = false;
+		int cnt = ss.update("mem.delMem", vo);
+		
+		if(cnt>0) {
+			chk = true;
+		}
+		return chk;
+	}
+	
+	
+	//회원 수정
+	public boolean updateMem(MemberVO vo) {
+		boolean chk = false;
+		int cnt = ss.update("updateMem",vo);
+		
+		if(cnt>0) {
+			chk = true;
+		}
+		return chk;
+	}
+	
+	//회원 로그인
+	public MemberVO login(String m_pw, String m_id) {
+		
+		Map<String, String>map = new HashMap<String, String>();	
+		map.put("m_id", m_id);
+		map.put("m_pw", m_pw);
+		MemberVO vo = ss.selectOne("mem.login", map);
+		
+		return vo;
+	}
+	
+	
+
+	
+	
+	
 	
 	
 }
--- src/main/java/mybatis/dao/ReviewDAO.java
+++ src/main/java/mybatis/dao/ReviewDAO.java
@@ -3,9 +3,47 @@ package mybatis.dao;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.data.vo.ReviewVO;
+
 public class ReviewDAO {
 	@Autowired
 	private SqlSessionTemplate ss;
+	
+	
+	//댓글 쓰기
+	public boolean writeReview(ReviewVO vo) {
+		boolean chk = false;
+		int cnt = ss.update("review.writeReview",vo);
+		if(cnt > 0) {
+			chk = true;
+		}
+		return chk;
+	}
+	
+	
+	//댓글 수정
+	public boolean updateReview(ReviewVO vo) {
+		boolean chk = false;
+		int cnt = ss.update("review.updateReview", vo);
+		
+		if(cnt > 0) {
+			chk = true;
+		}
+		return chk;
+	}
+	
+	
+	//댓글 삭제
+	public boolean delReview(String r_idx) {
+		boolean chk = false;
+		int cnt = ss.update("review.delReview", r_idx);
+		
+		if(cnt >0) {
+			chk= true;
+		}
+		
+		return chk;
+	}
 	
 	
 }
